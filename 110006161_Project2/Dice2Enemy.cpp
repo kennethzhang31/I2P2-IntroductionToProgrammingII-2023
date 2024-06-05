d8f6131b54428b720e876469f06edca46963bc76
kennethzhang31 <100484973+kennethzhang31@users.noreply.github.com>
2024-06-06 02:46:01 +0800

    Delete 110006161_Project2 directory

--- 110006161_Project2/Dice2Enemy.cpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-#include "Dice2Enemy.hpp"
-
-Dice2Enemy::Dice2Enemy(int x, int y) : Enemy("play/dice-2.png", x, y, 25, 50, 12, 15, 2) {
-    // TODO 2 (1/3): You can imitate the 2 files: 'RedNormalEnemy.hpp', 'RedNormalEnemy.cpp' to create a new enemy.
-}
--- 110006161_Project2/RedNormalEnemy.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef REDNORMALENEMY_HPP
-#define REDNORMALENEMY_HPP
-#include "Enemy.hpp"
-
-class RedNormalEnemy : public Enemy {
-public:
-    RedNormalEnemy(int x, int y);
-};
-#endif // REDNORMALENEMY_HPP
